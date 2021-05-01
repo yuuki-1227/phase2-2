1669b557236e4889826227cdec514a864f960a68
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-01 09:49:27 +0000

    [Update]Book一覧表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,7 +43,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,22 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def edit
     @user = User.find(params[:id])
+    
   end
 
   def update
